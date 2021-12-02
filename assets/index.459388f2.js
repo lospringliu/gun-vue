175bebd37117fe9d775d4b0c280d7b3aa1418db0
davay <fond@fondfrukt.ru>
2021-12-02 22:05:59 +0300

    deploy

---
+++ assets/index.459388f2.js
@@ -0,0 +1,1 @@
+import{_ as e,a as n}from"./index.3d6ad9ff.js";import{o as r,f as a}from"./vendor.d55da865.js";function c(s,_){const o=e;return r(),a(o)}const t={};var i=n(t,[["render",c]]);export{i as default};
