2f101d60b37ed5a58a933f8f78a1030136c972e4
davay <fond@fondfrukt.ru>
2021-12-09 11:11:46 +0300

    deploy

--- assets/index.c4f32e8e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as n}from"./index.7a9db77d.js";import{o as r,f as a}from"./vendor.9df87a26.js";function c(s,_){const o=e;return r(),a(o)}const t={};var i=n(t,[["render",c]]);export{i as default};
